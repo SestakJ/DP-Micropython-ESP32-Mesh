cad757b24f439c751b91ff61d7d0c1c0c14fce21
Jindrich Sestak <jindra.sestakk@gmail.com>
2022-04-13 21:25:22 +0200

    WifiCore receive msg in one func, register MAC address, working with MAC addresses

--- src/wificore.py
+++ src/wificore.py
@@ -48,7 +48,7 @@ class WifiCore():
         self._id = mac_to_str(self.ap.wlan.config('mac'))
         # User defined from config.json.
         self._loop = self.core._loop
-        self.children_writers = {}
+        self.children_writers = {} # {mac: (writer, (ip, port))}
         self.parent = self.parent_reader = self.parent_writer = None
         self.json_topology = self._config.get('topology', None)
 
@@ -69,7 +69,6 @@ class WifiCore():
             print(e)
         #     import machine
         #     # machine.reset() # TODO uncomment. It solves the problem with ERROR 104 ECONNRESET after Soft Reset on child.
-
 
     async def _run(self):
         """
@@ -95,9 +94,9 @@ class WifiCore():
             await self.sta.do_connect(self.sta_ssid, self.sta_password)
             self.dprint("[Connect to parent WiFi] Done")
         else:
+            # TODO here update topology of me becouase i am root node
             return
-        self.parent = self.sta.ifconfig()[2]
-        self.parent_reader, self.parent_writer = await asyncio.open_connection(self.parent, SERVER_PORT)
+        self.parent_reader, self.parent_writer = await asyncio.open_connection(self.sta.ifconfig()[2], SERVER_PORT)
         self.dprint("[Open connection to parent] Done")
         self._loop.create_task(self.send_to_parent())
         self._loop.create_task(self.receive_from_parent())
@@ -106,25 +105,15 @@ class WifiCore():
         msg = TopologyExchange(self._id, "ffffffffffff", 1, self.json_topology)
         while True:
             self.dprint("[SEND] to parent")
-            await self.send_msg(self.parent, msg)
+            await self.send_msg(self.parent , self.parent_writer, msg)
             await asyncio.sleep(15)
             
     async def receive_from_parent(self):
-        while True:
-            try:
-                res = await self.parent_reader.readline()
-                self.dprint("[Receive] from parent message: ", res)
-            except Exception as e:
-                self.dprint("[Receive] parent conn is prob dead, stop listening. Error: ", e)
-                res = b''
-                raise e
-            if res == b'': # Connection closed by host, clean up. Maybe hard reset.
-                await self.close_connection(self.parent)
-                self.dprint("[Receive] conn is dead")
-                return
-            #TODO create task for processing the message.
-            self._loop.create_task(self.send_to_children_once(res)) # Relay messages to children.
-            await asyncio.sleep(2)
+        self.parent = await self.register_mac(self.parent_reader) # Register peer with mac address
+        self._loop.create_task(self.receive(self.parent_reader))
+        #     #TODO create task for processing the message.
+        #     self._loop.create_task(self.send_to_children_once(res)) # Relay messages to children.
+        #     await asyncio.sleep(2)
             
     async def start_parenting(self):
         """
@@ -152,62 +141,77 @@ class WifiCore():
         # children = [mac[0] for mac in l]
         # TODO Must be SRC MAC and DST MAC in JSON messages, then I can match the mac addresses to the right IP addresses.
         print("[SEND] to children")
-        for destination in self.children_writers: # But this is tuple(IP, port)
+        for destination, writers in self.children_writers.items(): # But this is tuple(IP, port)
             # if destination[0] not in children: # Delete old connections from dead child nodes.
             #     # self._loop.create_task(self.close_connection(destination))
             #     continue
-            self._loop.create_task(self.send_msg(destination, msg))
+            self._loop.create_task(self.send_msg(destination, writers[0], msg))
 
     async def receive_from_child(self, reader, writer):
-        self.children_writers[writer.get_extra_info('peername')] = writer
-        self.dprint("[Receive] child added: ", writer.get_extra_info('peername'))
+        mac = await self.register_mac(reader) # Register peer with mac address.
+        self.children_writers[mac] = (writer, writer.get_extra_info('peername'))
+        self.dprint("[Receive] child added: ",msg["src"], writer.get_extra_info('peername'))
+        self._loop.create_task(self.receive(reader))
+
+    async def register_mac(self, reader):
+        """
+        Receive first blank packet to be able to register MAC address of node
+        """
+        try:
+            res = await reader.readline()
+            self.dprint("[Receive] from x message: ", res)
+            if res == b'': # Connection closed by host, clean up. Maybe hard reset.
+                await self.close_connection(writer.get_extra_info('peername'))
+                self.dprint("[Receive] conn is dead")
+                return
+        except Exception as e:
+            self.dprint("[Receive] x conn is prob dead, stop listening. Error: ", e)
+            return
+            raise e
+        msg = json.loads(res)
+        return msg["src"]
+
+    async def receive(self, reader):
+        # TODO must be a routing table and when dst address is not myself retransmit in direction of dst address up or down stream.
         try:
             while True:
-                data = await reader.readline()
-                addr = writer.get_extra_info('peername')
-                self.dprint(f"[Receive] from node {addr} message {data}")
-                if data == b'': # Connection closed by host. Close children
+                res = await reader.readline()
+                self.dprint("[Receive] from x message: ", res)
+                if res == b'': # Connection closed by host, clean up. Maybe hard reset.
                     await self.close_connection(writer.get_extra_info('peername'))
+                    self.dprint("[Receive] conn is dead")
                     return
         except Exception as e:
-            self.dprint("[Receive] child conn is prob dead, stop listening. Error: ", e)
-            await self.close_connection(writer.get_extra_info('peername'))
+            self.dprint("[Receive] x conn is prob dead, stop listening. Error: ", e)
             return
             raise e
         # self.dprint("Close the connection")
         # del self.children_writers[writer.get_extra_info('peername')]
         # writer.close()
         # await writer.wait_closed()
-    
-    async def send_msg(self, destination, message):
-        if not destination:
-            return
-        writer = None
-        if destination in self.children_writers:
-            writer = self.children_writers[destination]
-        elif destination == self.parent:
-            writer = self.parent_writer
-        else:
+
+    async def send_msg(self, mac, writer, message):
+        if not writer:
             return
         try:
-            self.dprint("[SEND] to :", destination, writer, " message: ", message)
+            self.dprint("[SEND] to:", mac ," message: ", message)
             writer.write('{}\n'.format(pack_wifimessage(message)))
             await writer.drain()
             print("[SEND] drained and done")
         except Exception as e:
             print("[Send] Whew! ", e, " occurred.")
-            await self.close_connection(destination)
+            await self.close_connection(mac)
     
-    async def close_connection(self, node):
+    async def close_connection(self, mac):
         writer = None
-        if node == self.parent:
+        if mac == self.parent:
             writer = self.parent_writer
             self.parent = None
             self.parent_writer = None
             self.parent_reader = None
-        elif node in self.children_writers:
-            writer = self.children_writers[node]
-            del self.children_writers[node]
+        elif mac in self.children_writers:
+            writer, ip = self.children_writers[mac]
+            del self.children_writers[mac]
         if writer:
             writer.close()
             await writer.wait_closed()
